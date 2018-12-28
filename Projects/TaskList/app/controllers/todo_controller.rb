f673e09409fda574b3bbf2301715ee327356c76f
maxwell <nintemod98@gmail.com>
2018-12-28 17:23:24 +0300

    123

--- Projects/TaskList/app/controllers/todo_controller.rb
+++ Projects/TaskList/app/controllers/todo_controller.rb
@@ -6,6 +6,9 @@ class TodoController < ApplicationController
   def update
   end
   def create
-      
+    project = Project.find(params['project_id'])
+    todo = Todo.create text: params['text'], isCompleted: false
+    project.todos << todo
+    redirect_to '/'
   end
 end
--- Projects/TaskList/config/routes.rb
+++ Projects/TaskList/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'todo#index'
-  get 'todo/update'
-  get 'todo/create'
+  post 'todo/update'
+  post 'todo/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
