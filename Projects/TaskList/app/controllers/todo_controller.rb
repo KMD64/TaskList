0df3e3f5714eed8f35d7956658f2ddd6914672cc
maxwell <nintemod98@gmail.com>
2019-01-01 19:32:22 +0300

    Several changes in conttroller

--- Projects/TaskList/app/controllers/todo_controller.rb
+++ Projects/TaskList/app/controllers/todo_controller.rb
@@ -1,7 +1,10 @@
 class TodoController < ApplicationController
   def index
-      @projects = Project.all.to_json
-    
+      @projects = Project.all
+      for @prj in @projects do
+         @prj.todos = Todo.where project_id: @prj.id 
+      end
+      @projects = @projects.to_json
   end
   def update
       todo = Todo.find(params['todo_id'])
