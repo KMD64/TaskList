36fa39210997918a403d8399fc2808892c5b330c
maxwell <nintemod98@gmail.com>
2019-01-02 15:16:54 +0300

    Minor changes in controller

--- Projects/TaskList/app/controllers/todo_controller.rb
+++ Projects/TaskList/app/controllers/todo_controller.rb
@@ -1,13 +1,18 @@
 class TodoController < ApplicationController
   def index
       @projects = Project.all
-      for prj in @projects do
-          prj.todos = Todo.where project_id: prj.id
-      end
-      respond_to do |format|
-        format.json{render json: @projects}
-        format.html
-      end
+      
+        respond_to do |format|
+          format.json{render json: @projects}
+          format.html
+        end
+  end
+  def gettodolist
+    @todos = Todo.where project_id: params['project_id']
+    respond_to do |format|
+        format.json{render json: @todos}
+        format.html{render html: @todos}
+    end
   end
   def update
       todo = Todo.find(params['todo_id'])
--- Projects/TaskList/config/routes.rb
+++ Projects/TaskList/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   root to: 'todo#index'
-  post 'todo/update'
-  post 'todo/create'
+  post 'update', to: 'todo#update'
+  post 'create', to: 'todo#create'
+  get 'gettodolist', to:'todo#gettodolist'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
